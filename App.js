a1a815116fa268abf0d0172195f518bd4f3efb2d
Nik <31962595+starscreamnik@users.noreply.github.com>
2022-03-18 11:18:06 +0200

    create

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import MainScreen from './src/main_screen';
+
+const App = MainScreen;
+
+export default App;
